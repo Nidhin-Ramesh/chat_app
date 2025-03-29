fc4ab1c7f7e4a315cd52dc9ddb94133fca0cb306
Nidhin Ramesh <nidhinramesh4@gmail.com>
2025-03-30 03:29:39 +0530

    completed the updateProfile function

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,15 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import cors from "cors"
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingPage = () => {
+  return (
+    <div>
+      settings
+    </div>
+  )
+}
+
+export default SettingPage
